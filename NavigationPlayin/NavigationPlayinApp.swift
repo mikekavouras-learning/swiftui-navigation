588625dab3e8d134928d693c71ccf26d5c23b537
Mike Kavouras <kavourasm@gmail.com>
2023-06-19 20:48:55 -0400

    Initial Commit

---
+++ NavigationPlayin/NavigationPlayinApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPlayinApp.swift
+//  NavigationPlayin
+//
+//  Created by Mike Kavouras on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPlayinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
